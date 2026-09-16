2b8666619c4633589cfe8f3fc33fb0209e6e96c6
Ngô Bá Hoàng <289644758+tikl20tok@users.noreply.github.com>
2026-09-16 07:57:28 +0700

    Solution for this

--- MarisaOJ/Roadmap - Level 0/String/String occurences.cpp
+++ MarisaOJ/Roadmap - Level 0/String/String occurences.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 #define ll long long
 
-
-
 int main()
 {
     ios::sync_with_stdio(false);
@@ -11,6 +9,37 @@ int main()
 
     freopen("TESTING.INP", "r", stdin);
     freopen("TESTING.OUT", "w", stdout);
+
+    string s, t;
+    cin>>s>>t;
+    ll i, j;
+    //kmp algorithm
+    string ts = t + "#" + s;
+    vector<ll> lps(ts.size()+5, 0);
+    for(i=1;i<ts.size();i++){
+        j = lps[i-1];
+        while(j > 0  &&  ts[i] != ts[j]){
+            j = lps[j-1];
+        }
+        if(ts[i] == ts[j]){
+            j++;
+        }
+        lps[i] = j;
+    }
+    ll dem = 0;
+    for(i=1;i<ts.size();i++){
+        if(lps[i] == t.size()){
+            dem++;
+        }
+    }
+    cout<<dem;
+
+    /*
+    cout<<'\n';
+    for(i=0;i<ts.size();i++){
+        cout<<lps[i]<<" ";
+    }
+    */
 
     
     return 0;
