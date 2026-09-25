ff95ae677bc599d1f91f5859259615fa3767c33d
Ngô Bá Hoàng <289644758+tikl20tok@users.noreply.github.com>
2026-09-25 22:42:04 +0700

    change name+temp

---
+++ CSP/Đếm số K ước.cpp
@@ -0,0 +1,18 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    freopen("TESTING.INP","r",stdin);
+    freopen("TESTING.OUT","w",stdout);
+
+    
+
+    return 0;
+}
